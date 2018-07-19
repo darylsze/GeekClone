d11115f5d0d6525a782ae483a0d8a86d1e3a6369
darylsze <darylsze@hk01.com>
2018-07-20 00:24:51 +0800

    feat: read file

--- gitclone.py
+++ gitclone.py
@@ -1,4 +1,10 @@
+config  = open("example_config", "r")
 
+print config.read()
+
+userEmail = raw_input("your email: ")
+
+# print('Your email is ' + userEmail)
 # write file
-with open('config.text', 'a') as the_file:
-    the_file.write('Hello\n')
+# with open('config.text', 'a') as the_file:
+    # the_file.write('Hello\n')
